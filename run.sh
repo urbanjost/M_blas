9e2bbb0134778e66b99140ed43cc9dd10f847a6f
John S. Urban <urbanjost@comcast.net>
2021-08-15 22:15:25 -0400

    ENDDO versus CONTINUE

--- run.sh
+++ run.sh
@@ -20,5 +20,19 @@ fpm test sblat3    < test/sblat3.in
 fpm test zblat1  
 fpm test zblat2    < test/zblat2.in
 fpm test zblat3    < test/zblat3.in
-)
+
+cat cblat2.out
+cat cblat3.out
+cat dblat2.out
+cat dblat3.out
+cat sblat2.out
+cat sblat3.out
+cat zblat2.out
+cat zblat3.out
+
+rm -f cblat2.out cblat3.out dblat2.out dblat3.out sblat2.out sblat3.out zblat2.out zblat3.out
+
+)|tee run.log
+
+grep -i fail run.log
 exit
