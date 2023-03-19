bf1aecc45422663dbe9da00d8303684408f36d3e
John S. Urban <urbanjost@comcast.net>
2023-03-19 09:45:22 -0400

    correct test for ifort/ifx compiler

--- test/dblat3_a.f90
+++ test/dblat3_a.f90
@@ -382,59 +382,8 @@
 !
 !     End of DBLAT3.
 !
-      contains
-      SUBROUTINE test_XERBLA( SRNAME, INFO )
-!
-!  This is a special version of XERBLA to be used only as part of
-!  the test program for testing error exits from the Level 3 BLAS
-!  routines.
-!
-!  XERBLA  is an error handler for the Level 3 BLAS routines.
-!
-!  It is called by the Level 3 BLAS routines if an input parameter is
-!  invalid.
-!
-!  Auxiliary routine for test program for Level 3 Blas.
-!
-!  -- Written on 8-February-1989.
-!     Jack Dongarra, Argonne National Laboratory.
-!     Iain Duff, AERE Harwell.
-!     Jeremy Du Croz, Numerical Algorithms Group Ltd.
-!     Sven Hammarling, Numerical Algorithms Group Ltd.
-!
-!     .. Scalar Arguments ..
-      CHARACTER(len=*),intent(in) :: SRNAME
-      INTEGER,intent(in)          :: INFO
-!     .. Scalars in Common ..
-      INTEGER            INFOT, NOUT
-      LOGICAL            LERR, OK
-      CHARACTER*6        SRNAMT
-!     .. Common blocks ..
-      COMMON             /INFOC/INFOT, NOUT, OK, LERR
-      COMMON             /SRNAMC/SRNAMT
-!     .. Executable Statements ..
-      LERR = .TRUE.
-      IF( INFO.NE.INFOT )THEN
-         IF( INFOT.NE.0 )THEN
-            WRITE( NOUT, FMT = 9999 )INFO, INFOT
-         ELSE
-            WRITE( NOUT, FMT = 9997 )INFO
-         END IF
-         OK = .FALSE.
-      END IF
-      IF( SRNAME.NE.SRNAMT )THEN
-         WRITE( NOUT, FMT = 9998 )SRNAME, SRNAMT
-         OK = .FALSE.
-      END IF
-      RETURN
-!
- 9999 FORMAT( ' ******* XERBLA WAS CALLED WITH INFO = ', I6, ' INSTEAD OF ', I2, ' *******' )
- 9998 FORMAT( ' ******* XERBLA WAS CALLED WITH SRNAME = ', A6, ' INSTEAD OF ', A6, ' *******' )
- 9997 FORMAT( ' ******* XERBLA WAS CALLED WITH INFO = ', I6, ' *******' )
-!
-!     End of XERBLA
-!
-      END SUBROUTINE test_XERBLA
+contains
+include "test_xerbla.inc90"
 
       END program
       SUBROUTINE DCHK1( SNAME, EPS, THRESH, NOUT, NTRA, TRACE, REWI, &
--- test/sblat2_a.f90
+++ test/sblat2_a.f90
@@ -429,60 +429,8 @@
 !
 !     End of SBLAT2.
 !
-      contains
-      SUBROUTINE test_XERBLA( SRNAME, INFO )
-!
-!  This is a special version of XERBLA to be used only as part of
-!  the test program for testing error exits from the Level 2 BLAS
-!  routines.
-!
-!  XERBLA  is an error handler for the Level 2 BLAS routines.
-!
-!  It is called by the Level 2 BLAS routines if an input parameter is
-!  invalid.
-!
-!  Auxiliary routine for test program for Level 2 Blas.
-!
-!  -- Written on 10-August-1987.
-!     Richard Hanson, Sandia National Labs.
-!     Jeremy Du Croz, NAG Central Office.
-!
-!     .. Scalar Arguments ..
-      INTEGER,intent(in)          :: INFO
-      CHARACTER(len=6),intent(in) :: SRNAME
-!     .. Scalars in Common ..
-      INTEGER            INFOT, NOUT
-      LOGICAL            LERR, OK
-      CHARACTER*6        SRNAMT
-!     .. Common blocks ..
-      COMMON             /INFOC/INFOT, NOUT, OK, LERR
-      COMMON             /SRNAMC/SRNAMT
-!     .. Executable Statements ..
-      LERR = .TRUE.
-      IF( INFO.NE.INFOT )THEN
-         IF( INFOT.NE.0 )THEN
-            WRITE( NOUT, FMT = 9999 )INFO, INFOT
-         ELSE
-            WRITE( NOUT, FMT = 9997 )INFO
-         END IF
-         OK = .FALSE.
-      END IF
-      IF( SRNAME.NE.SRNAMT )THEN
-         WRITE( NOUT, FMT = 9998 )SRNAME, SRNAMT
-         OK = .FALSE.
-      END IF
-      RETURN
-!
- 9999 FORMAT( ' ******* XERBLA WAS CALLED WITH INFO = ', I6, ' INSTEAD', &
-     &      ' OF ', I2, ' *******' )
- 9998 FORMAT( ' ******* XERBLA WAS CALLED WITH SRNAME = ', A6, ' INSTE', &
-     &      'AD OF ', A6, ' *******' )
- 9997 FORMAT( ' ******* XERBLA WAS CALLED WITH INFO = ', I6, &
-     &      ' *******' )
-!
-!     End of XERBLA
-!
-      END SUBROUTINE test_XERBLA
+contains
+include "test_xerbla.inc90"
       SUBROUTINE SCHK1( SNAME, EPS, THRESH, NOUT, NTRA, TRACE, REWI, &
      &                  FATAL, NIDIM, IDIM, NKB, KB, NALF, ALF, NBET, &
      &                  BET, NINC, INC, NMAX, INCMAX, A, AA, AS, X, XX, &
